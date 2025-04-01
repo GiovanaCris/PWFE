bc781ef5d425b23f8aa1a13eacd4c0bc6cbe58eb
GiovanaCris <giovana.bp@outlook.com>
2025-04-01 01:29:26 -0300

    atvs fer atualizada 31-03

---
+++ Entrega Fer2/portifolio atividades1/script.js
@@ -0,0 +1,44 @@
+const video = document.getElementById('video_fundo');
+video.playbackRate = 0.1; // Velocidade do vídeo de fundo
+
+function minimizar(tipo) {
+    let uiuxBox = document.getElementById("uiux-box");
+    let uiuxAtividades = document.getElementById("atividades");
+
+    let frontendBox = document.getElementById("frontend-box");
+    let frontendAtividades = document.getElementById("atividades2");
+
+    let conteudoInicial = document.getElementById("content_page");
+
+    if (tipo === "uiux") {
+        let uiuxAberto = !uiuxAtividades.classList.contains("hidden");
+        uiuxAtividades.classList.add("hidden");
+        frontendAtividades.classList.add("hidden2");
+        conteudoInicial.classList.remove("hidden");
+
+        uiuxBox.classList.remove("minimized");
+        frontendBox.classList.remove("minimized2");
+
+        if (!uiuxAberto) {
+            uiuxAtividades.classList.remove("hidden");
+            conteudoInicial.classList.add("hidden");
+            uiuxBox.classList.add("minimized");
+            frontendBox.classList.remove("minimized2");
+        }
+    } else if (tipo === "frontend") {
+        let frontendAberto = !frontendAtividades.classList.contains("hidden2");
+        uiuxAtividades.classList.add("hidden");
+        frontendAtividades.classList.add("hidden2");
+        conteudoInicial.classList.remove("hidden");
+
+        uiuxBox.classList.remove("minimized");
+        frontendBox.classList.remove("minimized2");
+
+        if (!frontendAberto) {
+            frontendAtividades.classList.remove("hidden2");
+            conteudoInicial.classList.add("hidden");
+            frontendBox.classList.add("minimized2");
+            uiuxBox.classList.remove("minimized");
+        }
+    }
+}
